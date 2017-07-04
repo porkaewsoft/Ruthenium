79d5be31a35701de4bfe61dd12214af4b59ce7e1
Peerachet Porkaew <peerachet.porkaew@gmail.com>
2017-07-04 14:01:18 +0800

    Training

--- main_SimpleRNN02.py
+++ main_SimpleRNN02.py
@@ -1,6 +1,10 @@
 
 """
 An implementation of Recurrent Nueral Network for Sequence Labeling
+
+Running 
+THEANO_FLAGS='floatX=float32' python main_SimpleRNN02.py
+
 """
 
 import utils
@@ -37,29 +41,42 @@ class Model_SimpleRNN:
 
         logging.info('Start Building Model...')
 
+        logging.info("config.floatX" + " " + config.floatX)
+
+        TparamD = OrderedDict()
+
         trg_vocab_size = 20
         src_vocab_size = 20
         hidden_size = 20
         embedding_size = 5
 
         self.x = T.matrix("x",dtype="int64")
+        self.x0 = self.x
         x = self.x
         inp = self.x.dimshuffle((1,0))
 
-        self.layer1 = EmbeddingLayer(inp, vocab_size=src_vocab_size, embedding_size=embedding_size)
+        self.layer1 = EmbeddingLayer(inp, vocab_size=src_vocab_size, embedding_size=embedding_size,prefix="Embed01")
         self.forward1 = theano.function([x], self.layer1.output)
 
+
+
         self.layer2 = SimpleRNNLayer(inpSeq=self.layer1.output,mask=None,in_dim=embedding_size,
-                                    hidden_dim=hidden_size,bias=1)
+                                    hidden_dim=hidden_size,bias=1, prefix="RNN01")
 
         self.forward2 = theano.function([x], self.layer1.output)        
         self.current_output = self.layer2.output[0]
 
 
-        self.layer3 = TimeDistributedDenseLayer(inpSeq=self.current_output,mask=None,in_dim=hidden_size,out_dim=trg_vocab_size,activation="softmax")
+        self.layer3 = TimeDistributedDenseLayer(inpSeq=self.current_output,mask=None,in_dim=hidden_size,out_dim=trg_vocab_size,activation="softmax",prefix="TimeDense01")
         self.forward = theano.function([x],self.layer3.output)
 
+        TparamD = OrderedDict()
+        TparamD.update(self.layer1.Tparam)
+        TparamD.update(self.layer2.Tparam)
+        TparamD.update(self.layer3.Tparam)
 
+
+        print TparamD
         #Training step 
         """
             Batch of probs  of each time step
@@ -70,8 +87,8 @@ class Model_SimpleRNN:
 
         """
     
-        self.y = T.matrix("y",dtype="int64")
-        self.y = self.y.dimshuffle((1,0))
+        self.y0 = T.matrix("y",dtype="int64")
+        self.y = self.y0.dimshuffle((1,0))
 
         probs = self.layer3.output #layer2.output is already softmax
         y_flat = self.y.flatten() #flatten ids
@@ -87,8 +104,18 @@ class Model_SimpleRNN:
         Add Regularize HERE !!
         """
 
+        logging.info("Building Gradient...")
         self.train = theano.function([self.x,self.y],[probs,cost])
 
+        UpdateParams = TparamD
+
+        grads = T.grad(cost, wrt=list(UpdateParams.values()))
+        f_grad = theano.function([self.x0, self.y0], grads, name='f_grad')
+
+        lr = T.scalar(name='lr')
+        optimizer = adadelta
+        self.f_grad_shared, self.f_update = optimizer(lr, UpdateParams, grads,
+                                                                   self.x0, self.y0, cost)
 
         logging.info('Building Model Completed.')
 
@@ -113,7 +140,9 @@ def batch_generator(srcL,trgL,batch_size=20):
 
 
 if __name__ == "__main__":
-
+    """
+    Load Corpus -- read src and trg, then convert to ids
+    """
     logging.info("Loading Corpus...")
     srcL,trgL = load_corpus("data/src.txt","data/trg.txt","data/src.vocab","data/trg.vocab")
     
@@ -124,12 +153,19 @@ if __name__ == "__main__":
     model = Model_SimpleRNN()  
 
     i = 0
+    lrate = 0.01
+    nepoch = 10
 
-    for src,trg in batch_generator(np_srcL,np_trgL):
-        out = model.train(src,trg)
-        print i,out[1]
-        i += 1
+    for n in range(nepoch):
+        total_loss = 0.0
+        for src,trg in batch_generator(np_srcL,np_trgL):
+            loss = model.f_grad_shared(src,trg)
+            total_loss += loss
+            model.f_update(lrate)
+            i += 1
+        logging.info("Epoch %d %f"%(n,total_loss))
 
     #model = Model_SimpleRNN()  
 
     #out = model.predict(np.asarray([[1,2],[2,3],[3,1]],dtype="int64"))
+
